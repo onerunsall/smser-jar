dcbb0ecfbc6d4b08f62148b618088888462880fe
renxinwei <413038044@qq.com>
2019-04-08 15:33:10 +0800

    1

---
+++ src/sms/launcher/Constant.java
@@ -0,0 +1,16 @@
+package sms.launcher;
+
+public class Constant {
+	String sms2SendUrl = "http://127.0.0.1:8083/sms/sms2/send";
+	String sms2SendTemplateUrl = "http://127.0.0.1:8083/sms/sms2/sendtemplate";
+	String verification2SendUrl = "http://127.0.0.1:8083/sms/verification2/send";
+	String verification2SendDiyUrl = "http://127.0.0.1:8083/sms/verification2/senddiy";
+	String verification2VerifyUrl = "http://127.0.0.1:8083/sms/verification2/verify";
+	String client = null;
+	String environment = null;
+	String CHARSET = "utf-8";
+
+	Constant() {
+		// TODO Auto-generated constructor stub
+	}
+}
