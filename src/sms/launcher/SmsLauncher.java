0dd6ff4fd11760ac947dbb0f643547436f5394e8
renxinwei <413038044@qq.com>
2019-04-11 11:21:30 +0800

    1

--- src/sms/launcher/SmsLauncher.java
+++ src/sms/launcher/SmsLauncher.java
@@ -1,5 +1,6 @@
 package sms.launcher;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,8 +16,8 @@ public class SmsLauncher {
 	private static Logger logger = Logger.getLogger(SmsLauncher.class);
 
 	MsLauncher msLauncher = null;
-	Map<String,String> msTpls = new HashMap();
-	
+	Map<String, String> msTpls = new HashMap();
+
 	public void registerMeisheng(String account, String password, String veryCode) {
 		msLauncher = new MsLauncher(account, password, veryCode);
 	}
@@ -24,13 +25,36 @@ public class SmsLauncher {
 	public void registerTpl(String tplCode, String msTplId) {
 		msTpls.put(tplCode, msTplId);
 	}
-	
+
 	public void sendSms(String mobile, String content) {
 		msLauncher.sendSms(mobile, content);
 	}
 
+	public void sendTplSms(List<String> mobiles, String tplCode, List<String> contents) {
+		String mobile = "";
+		for (int i = 0; i < mobiles.size(); i++) {
+			if (i == 0)
+				mobile = mobile + mobiles.get(i);
+			else
+				mobile = "," + mobile + mobiles.get(i);
+		}
+		msLauncher.sendTplSms(mobile, msTpls.get(tplCode), contents);
+	}
+
+	public void sendTplSms(String[] mobiles, String tplCode, List<String> contents) {
+		sendTplSms(Arrays.asList(mobiles), msTpls.get(tplCode), contents);
+	}
+
+	public void sendTplSms(String[] mobiles, String tplCode, String... contents) {
+		sendTplSms(Arrays.asList(mobiles), msTpls.get(tplCode), Arrays.asList(contents));
+	}
+
 	public void sendTplSms(String mobile, String tplCode, List<String> contents) {
 		msLauncher.sendTplSms(mobile, msTpls.get(tplCode), contents);
+	}
+
+	public void sendTplSms(String mobile, String tplCode, String... contents) {
+		sendTplSms(mobile, msTpls.get(tplCode), Arrays.asList(contents));
 	}
 
 }
