3813e678d6e3c19388f93a764d6a27e350779458
renxinwei <413038044@qq.com>
2019-04-17 16:47:56 +0800

    优化方法名，参数名

--- src/sms/launcher/SmsLauncher.java
+++ src/sms/launcher/SmsLauncher.java
@@ -22,15 +22,15 @@ public class SmsLauncher {
 		msLauncher = new MsLauncher(account, password, veryCode);
 	}
 
-	public void registerTpl(String tplCode, String msTplId) {
-		msTpls.put(tplCode, msTplId);
+	public void registerMeishengTpl(String businessCode, String msTplId) {
+		msTpls.put(businessCode, msTplId);
 	}
 
 	public void sendSms(String mobile, String content) {
 		msLauncher.sendSms(mobile, content);
 	}
 
-	public void sendTplSms(List<String> mobiles, String tplCode, List<String> contents) {
+	public void sendTplSms(List<String> mobiles, String businessCode, List<String> contents) {
 		String mobile = "";
 		for (int i = 0; i < mobiles.size(); i++) {
 			if (i == 0)
@@ -38,23 +38,23 @@ public class SmsLauncher {
 			else
 				mobile = "," + mobile + mobiles.get(i);
 		}
-		msLauncher.sendTplSms(mobile, msTpls.get(tplCode), contents);
+		msLauncher.sendTplSms(mobile, msTpls.get(businessCode), contents);
 	}
 
-	public void sendTplSms(String[] mobiles, String tplCode, List<String> contents) {
-		sendTplSms(Arrays.asList(mobiles), msTpls.get(tplCode), contents);
+	public void sendTplSms(String[] mobiles, String businessCode, List<String> contents) {
+		sendTplSms(Arrays.asList(mobiles), msTpls.get(businessCode), contents);
 	}
 
-	public void sendTplSms(String[] mobiles, String tplCode, String... contents) {
-		sendTplSms(Arrays.asList(mobiles), msTpls.get(tplCode), Arrays.asList(contents));
+	public void sendTplSms(String[] mobiles, String businessCode, String... contents) {
+		sendTplSms(Arrays.asList(mobiles), msTpls.get(businessCode), Arrays.asList(contents));
 	}
 
-	public void sendTplSms(String mobile, String tplCode, List<String> contents) {
-		msLauncher.sendTplSms(mobile, msTpls.get(tplCode), contents);
+	public void sendTplSms(String mobile, String businessCode, List<String> contents) {
+		msLauncher.sendTplSms(mobile, msTpls.get(businessCode), contents);
 	}
 
-	public void sendTplSms(String mobile, String tplCode, String... contents) {
-		sendTplSms(mobile, msTpls.get(tplCode), Arrays.asList(contents));
+	public void sendTplSms(String mobile, String businessCode, String... contents) {
+		sendTplSms(mobile, msTpls.get(businessCode), Arrays.asList(contents));
 	}
 
 }
