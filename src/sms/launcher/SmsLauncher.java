20ef0777be65c4b315de7f10603f3b823295d5b3
renxinwei <413038044@qq.com>
2019-05-17 16:18:18 +0800

    1

--- src/sms/launcher/SmsLauncher.java
+++ src/sms/launcher/SmsLauncher.java
@@ -31,6 +31,7 @@ public class SmsLauncher {
 	}
 
 	public void sendTplSms(List<String> mobiles, String businessCode, List<String> contents) {
+		logger.info(mobiles);
 		String mobile = "";
 		for (int i = 0; i < mobiles.size(); i++) {
 			if (i == 0)
@@ -38,6 +39,7 @@ public class SmsLauncher {
 			else
 				mobile = "," + mobile + mobiles.get(i);
 		}
+		logger.info(mobile);
 		msLauncher.sendTplSms(mobile, msTpls.get(businessCode), contents);
 	}
 
