2be2eeb15a66bf90ecdaffe0d0c85297e4210cb5
renxinwei <413038044@qq.com>
2019-05-17 16:35:20 +0800

    1

--- src/sms/launcher/SmsLauncher.java
+++ src/sms/launcher/SmsLauncher.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 
@@ -32,7 +34,13 @@ public class SmsLauncher {
 
 	public void sendTplSms(List<String> mobiles, String businessCode, List<String> contents) {
 		String mobile = "";
+		String regex = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\d{8}$";
+		Pattern p = Pattern.compile(regex);
 		for (int i = 0; i < mobiles.size(); i++) {
+			if (mobiles.get(i) == null || mobiles.get(i).length() != 11)
+				return;
+			if (!p.matcher(mobiles.get(i)).matches())
+				return;
 			if (i == 0)
 				mobile = mobile + mobiles.get(i);
 			else
